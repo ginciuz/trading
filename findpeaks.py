212bbcdb2cf432ce9d49a9c3a835cd8b73c596ff
ginciuz <gintaras.jovaisas@gmail.com>
2019-03-03 02:20:50 +0000

    Add files via upload

---
+++ findpeaks.py
@@ -0,0 +1,59 @@
+def findpeaks(series, DELTA):
+    """
+    http://docs.sunpy.org/en/stable/generated/gallery/time_series/timeseries_peak_finding.html
+    Finds extrema in a pandas series data.
+
+    Parameters
+    ----------
+    series : `pandas.Series`
+        The data series from which we need to find extrema.
+
+    DELTA : `float`
+        The minimum difference between data values that defines a peak.
+
+    Returns
+    -------
+    minpeaks, maxpeaks : `list`
+        Lists consisting of pos, val pairs for both local minima points and
+        local maxima points.
+    """
+    # Set inital values
+    mn, mx = np.Inf, -np.Inf
+    minpeaks = []
+    maxpeaks = []
+    lookformax = True
+    start = True
+    # Iterate over items in series
+    for time_pos, value in series.iteritems():
+        if value > mx:
+            mx = value
+            mxpos = time_pos
+        if value < mn:
+            mn = value
+            mnpos = time_pos
+        if lookformax:
+            if value < mx-DELTA:
+                # a local maxima
+                maxpeaks.append((mxpos, mx))
+                mn = value
+                mnpos = time_pos
+                lookformax = False
+            elif start:
+                # a local minima at beginning
+                minpeaks.append((mnpos, mn))
+                mx = value
+                mxpos = time_pos
+                start = False
+        else:
+            if value > mn+DELTA:
+                # a local minima
+                minpeaks.append((mnpos, mn))
+                mx = value
+                mxpos = time_pos
+                lookformax = True
+    # check for extrema at end
+    if value > mn+DELTA:
+        maxpeaks.append((mxpos, mx))
+    elif value < mx-DELTA:
+        minpeaks.append((mnpos, mn))
+    return minpeaks, maxpeaks
